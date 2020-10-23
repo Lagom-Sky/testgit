f275e1b4ac01fd16ad6b7a028ab4c57fbd9938c8
MR TANG <754752345@qq.com>
2020-10-23 19:49:59 +0800

    加油你是最棒的

--- src/main/java/com/java/hello/test.java
+++ src/main/java/com/java/hello/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("这里是testGit2发生的修改希望不要产生冲突");
         System.out.println("这里是testGit1发生冲突的地方");
         System.out.println("test git 2 ");
+        System.out.println("这是在git2 产生的修改 我希望我写的代码不是来及 我还有许多需要 学习的地方 加油 你是最棒的");
     }
 }
