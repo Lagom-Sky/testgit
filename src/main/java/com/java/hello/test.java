9f2615034beccfb14de3ff96c0e6bae5bb102c3d
MR TANG <754752345@qq.com>
2020-10-23 20:01:44 +0800

    new branch 3 发生的修改

--- src/main/java/com/java/hello/test.java
+++ src/main/java/com/java/hello/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("test git 2 ");
         System.out.println("这是在git2 产生的修改 我希望我写的代码不是来及 我还有许多需要 学习的地方 加油 你是最棒的");
         System.out.println("这里test git 三发生的修改我希望你可以接受 加油你好 你是最棒的");
+        System.out.println("这里是新的分支new branch 产生的修改 希望你是最好的加油 加油不知道以后的生活应该何去何从");
     }
 }
