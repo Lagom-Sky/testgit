e0a85201efbebc692f322afe8448f79a2ca022e7
MR TANG <754752345@qq.com>
2020-10-23 19:26:40 +0800

    这里是testGit1发生修改后的提交希望你门可以不要让这里产生冲突

--- src/main/java/com/java/hello/test.java
+++ src/main/java/com/java/hello/test.java
@@ -3,5 +3,6 @@ package com.java.hello;
 public class test {
     public static void main(String[] args) {
         System.out.println("加油你好test git");
+        System.out.println("这里是testGit1发生冲突的地方");
     }
 }
