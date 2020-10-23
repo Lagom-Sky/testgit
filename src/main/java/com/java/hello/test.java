4d7e222548f1fe024084c78a81ef521e237c58b4
MR TANG <754752345@qq.com>
2020-10-23 19:34:48 +0800

    test git 2

--- src/main/java/com/java/hello/test.java
+++ src/main/java/com/java/hello/test.java
@@ -3,6 +3,8 @@ package com.java.hello;
 public class test {
     public static void main(String[] args) {
         System.out.println("加油你好test git");
+        System.out.println("这里是testGit2发生的修改希望不要产生冲突");
         System.out.println("这里是testGit1发生冲突的地方");
+        System.out.println("test git 2 ");
     }
 }
