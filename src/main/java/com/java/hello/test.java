e3291645ad463d1cb28b3a7aa7b3f8611f7d720b
MR TANG <754752345@qq.com>
2020-10-23 19:59:36 +0800

    这里是test git 3 新建分支之后 发生的修改 加油啊 你是

--- src/main/java/com/java/hello/test.java
+++ src/main/java/com/java/hello/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("这里test git 3发生的修改我希望你可以接受 加油你好 你是最棒的");
         System.out.println("这里是new git branch git 3 发生的修改 希望不要有冲突发生谢天谢地啊加油你是最棒的");
         System.out.println("这里是新建分支后发生的修改希望你可以知道我是对的，我喜欢你美女");
+        System.out.println("这里新建分支之后发生的修改希望你可以知道，这是修改二");
     }
 }
